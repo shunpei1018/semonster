b01733acbe4ecd16ad1b1b00f6845a0ba55e37b4
shunpei <e1n22006@oit.ac.jp>
2025-06-18 18:23:35 +0900

    v1.1: メッセージを修正

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
-  }
+    public static void main(String[] args) {
+        System.out.println("SEMONSTER GAME");
+        System.out.println("Show a new monster!");
+        System.out.println("ドラゴン:レア度[0]");
+    }
 }
