56f150fa0f26c2c7405aadbfa51aa2a57cb362a7
shunpei <e1n22006@oit.ac.jp>
2025-06-18 18:01:47 +0900

    Playerを追加し、Mainから呼び出すように修正

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
   }
 }
